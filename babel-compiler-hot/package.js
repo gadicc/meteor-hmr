7e853b5bb33b3a140a9828245b4576681780e83b
Gadi Cohen <dragon@wastelands.net>
2016-02-06 19:18:50 +0200

    update package bases to 1.3-modules-beta.7 (b7c48ee8ed1c7b013396c8e555c09e6079af6eb4)

--- babel-compiler-hot/package.js
+++ babel-compiler-hot/package.js
@@ -6,11 +6,11 @@ Package.describe({
   // isn't possible because you can't publish a non-recommended
   // release with package versions that don't have a pre-release
   // identifier at the end (eg, -dev)
-  version: '6.4.0-modules.6'
+  version: '6.4.0-modules.7'
 });
 
 Npm.depends({
-  'meteor-babel': '0.6.6',
+  'meteor-babel': '0.7.1',
   'mongodb': '2.1.4',
   'babel-plugin-react-transform': '2.0.0'
 });
