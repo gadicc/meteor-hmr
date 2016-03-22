dd36b5610bb9fe10a02154dac7066c5b2e688da3
Gadi Cohen <dragon@wastelands.net>
2016-03-22 09:50:37 +0200

    update package bases to 1.3-rc.4 (ff7f59d) - no changes

--- ecmascript-hot/package.js
+++ ecmascript-hot/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'gadicc:ecmascript-hot',
-  // version: '0.4.1-rc.4'    // core version, KEEP UPDATED
+  // version: '0.4.1-rc.5'    // core version, KEEP UPDATED
   version: '0.0.11-rc.4',
   summary: 'Replacement ecmascript package providing react hotloading',
   git: 'https://github.com/gadicc/meteor-react-hotloader',
