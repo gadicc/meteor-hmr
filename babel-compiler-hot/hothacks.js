5f0f55ccc18bf03d876488d3f18280e84d6410bc
Gadi Cohen <dragon@wastelands.net>
2016-03-30 17:44:17 +0200

    really delete old bundles

--- babel-compiler-hot/hothacks.js
+++ babel-compiler-hot/hothacks.js
@@ -146,8 +146,8 @@ MongoClient.connect(url, function(err, db) {
   // console.info("[gadicc:hot - babel-compiler-hot] connected to db"); 
   hot.col = db.collection('__hot');
 
-  // on startup, delete bundles older than 10s
-  hot.col.deleteMany({ ctime: { $lt: Date.now() - 10000 }}, function(err) {
+  // on startup, delete bundles /* older than 10s */
+  hot.col.deleteMany({ /* ctime: { $lt: Date.now() - 10000 } */}, function(err) {
     if (err) console.log(err);
   });
 
