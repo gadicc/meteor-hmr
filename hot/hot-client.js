ca283a7df0de7f8de9e6b024d029ec749c14fb41
Gadi Cohen <dragon@wastelands.net>
2016-04-07 14:59:52 +0200

    websocket reconnection / resilliency

--- hot/hot-client.js
+++ hot/hot-client.js
@@ -23,16 +23,44 @@ var port = Meteor.settings.public.HOT_PORT;
 var wsUrl = 'ws://' + location.hostname + ':' + port + '/';
 var serverBase = 'http://' + location.hostname + ':' + port + '/hot.js?hash=';
 
-var ws = new WebSocket(wsUrl);
+var ws = {};
+
 ws.onmessage = function(event) {
   var script = document.createElement('script');
   script.type = 'text/javascript';
   script.src = serverBase + event.data; 
   document.head.appendChild(script);
 }
+
 ws.onopen = function() {
+  ws.reconnecting = false;
   console.log('[gadicc:hot] Connected and ready.');
 }
+
+/*
+ws.onerror = function(error) {
+  console.log('[gadicc:hot] Socket error', error);
+}
+*/
+
+ws.onclose = function() {
+  if (!ws.reconnecting) {
+    ws.reconnecting = true;
+    console.log('[gadicc:hot] Disconnected, attempting to reconnect...');
+  }
+  
+  setTimeout(ws.open, 100);
+}
+
+ws.open = function() {
+  ws.connection = new WebSocket(wsUrl);
+  ws.connection.onopen = ws.onopen;
+  ws.connection.onclose = ws.onclose;
+  ws.connection.onerror = ws.onerror;
+  ws.connection.onmessage = ws.onmessage;
+}
+
+ws.open();
 
 /*
  * Takes install.js root and flattens it, so we can easily access modules by
