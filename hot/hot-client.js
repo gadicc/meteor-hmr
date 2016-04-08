fcfaaa5741e5ee02b164246b7d433e7ef238041e
Gadi Cohen <dragon@wastelands.net>
2016-04-08 10:01:10 +0200

    hot-client: more debugging info for "Cannot read property 'c' of undefined" #29

--- hot/hot-client.js
+++ hot/hot-client.js
@@ -108,12 +108,21 @@ function resolvePath(moduleId, requirePath) {
  * Given install.js root and a meteorInstall tree, walk through the later
  * and call func with corresponding File and moduleCodeArray of each.
  */
-function walkFileTree(root, tree, func) {
+function walkFileTree(root, tree, func, oldRoot) {
   for (var key in tree) {
-    if (_.isArray(tree[key]))
+    if (_.isArray(tree[key])) {
+      if (!root) {
+        console.log('[gadicc:hot] no root for', key);
+        console.log('oldRoot.c', oldRoot.c);
+        console.log('tree[key]', tree[key]);
+        console.log("If this happens once, it's safe to Ctrl-R.  If it " +
+          'keeps happening, it would be great if you could open a github ' +
+          'issue with a link to a github repo and steps to reproduce.');
+      }
       func(root.c[key], tree[key]);
-    else
-      walkFileTree(root.c[key], tree[key], func);
+    }
+    else 
+      walkFileTree(root.c[key], tree[key], func, root);
   }
 }
 
