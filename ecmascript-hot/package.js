da134a445e518cbc5ad668dd597becf2a5cf59d3
Gadi Cohen <dragon@wastelands.net>
2016-04-10 12:56:34 +0200

    gadicc:ecmascript-hot@1.3.1-fast.6

--- ecmascript-hot/package.js
+++ ecmascript-hot/package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'gadicc:ecmascript-hot',
   // version: '0.4.2'    // core version, KEEP UPDATED
   // version: '1.3.1_1',
-  version: '1.3.1-fast.5',
+  version: '1.3.1-fast.6',
   summary: 'Replacement ecmascript package providing react hotloading',
   git: 'https://github.com/gadicc/meteor-react-hotloader',
   documentation: '../README.md'
@@ -18,7 +18,7 @@ Package.onUse(function (api) {
   api.versionsFrom('1.3.1');
   api.use('isobuild:compiler-plugin@1.0.0');
   // api.use('gadicc:babel-compiler-hot@6.6.1_1');
-  api.use('gadicc:babel-compiler-hot@=6.6.2-beta.4');
+  api.use('gadicc:babel-compiler-hot@6.6.2-beta.5');
 
   api.imply('modules');
   api.imply('ecmascript-runtime');
