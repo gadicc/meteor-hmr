5bd4fad6527a2af27f35010bb79697427ca96826
Gadi Cohen <dragon@wastelands.net>
2016-04-25 17:04:37 +0200

    ecmascript-hot@=1.3.2-refactor.6 uses hot-build@0.0.8 (#45, #51)

--- ecmascript-hot/package.js
+++ ecmascript-hot/package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'gadicc:ecmascript-hot',
   // version: '0.4.3'    // core version, KEEP UPDATED
   // version: '1.3.1_1',
-  version: '1.3.2-refactor.5',
+  version: '1.3.2-refactor.6',
   summary: 'Replacement ecmascript package providing react hotloading',
   git: 'https://github.com/gadicc/meteor-react-hotloader',
   documentation: '../README.md'
@@ -19,7 +19,7 @@ Package.onUse(function (api) {
   api.use('isobuild:compiler-plugin@1.0.0');
   // api.use('gadicc:babel-compiler-hot@6.6.1_1');
   api.use('gadicc:babel-compiler-hot@=6.6.2-babelrc.1');
-  api.use('gadicc:hot-build@0.0.6');
+  api.use('gadicc:hot-build@0.0.8');
 
   api.imply('modules');
   api.imply('ecmascript-runtime');
